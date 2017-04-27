b877a0c08eb7f76003d5a8a4ccea1f3c70cd504b
The Boss <tatchu.it@gmail.com>
2017-04-27 18:31:16 +0700

    Update gitigone

---
+++ server/app/config/development.conf.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let config = {};
+
+config.web = {
+    db: {
+        uri: 'mongodb://localhost/db_english_study',
+        options: {
+            user: '',
+            pass: '',
+            password: ''
+        },
+        prefixCollection: 'mhv_'
+    },
+    elasticsearch: {
+        ES_Sync: false,
+        config: {
+            hosts: [{
+                protocol: 'http',
+                host: 'localhost',
+                port: 9200,
+            }, ],
+            // log: 'trace',
+        }
+    },
+    context: {
+        settings: {
+            env: 'development', //'development', //'production',
+            facebookId: '427079307489462',
+            services: {
+                webUrl: '//localhost:9020',
+                admin: '//localhost:9021/admin',
+                userApi: '//localhost:9022/v1/api',
+                apiUrl: '//localhost:9022/v1/api',
+                logApi: '//localhost:9022/v1/api',
+                
+                contactApi: '//localhost:9022/v1/api',
+                uploadApi: '//localhost:9022/v1/api',
+
+                socketApi: '//localhost:9023/v1/api',
+            }
+        }
+    }
+};
+
+module.exports = config;

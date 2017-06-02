89caad07036840f6477b155bb78aa4fab13db1c1
The Boss <tatchu.it@gmail.com>
2017-06-02 13:38:58 +0700

    update report & epoch training

--- ocr/code/_training.js
+++ ocr/code/_training.js
@@ -5,13 +5,17 @@ var jsonfile = require('jsonfile')
 var file = 'net1.json'
 var shuffle = require('shuffle-array');
 
+const arraysChart = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'k', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 var net = new brain.NeuralNetwork({
-    hiddenLayers: 120,
+    hiddenLayers: 90,
     learningRate: 0.3
 });
 
 var trainingSet = require('./../tmp/data-training.json');
 var testSet = require('./../tmp/data-testing.json');
+
 
 shuffle(trainingSet)
 shuffle(testSet)
@@ -19,7 +23,7 @@ shuffle(testSet)
 console.time('train: ')
 net.train(trainingSet, {
     errorThresh: 0.0005,  // error threshold to reach
-    iterations: 20000,   // maximum training iterations
+    iterations: 10000,   // maximum training iterations
     log: true,           // console.log() progress periodically
     logPeriod: 1,       // number of iterations between logging
     learningRate: 0.2,   // learning rate
@@ -32,18 +36,27 @@ console.timeEnd('train: ')
 let totalSetTest = testSet.length;
 let OK = 0;
 
+let detailTest = {
+    count: new Array(26).fill(0), // số mẫu test của 26 ký tự
+    ok: new Array(26).fill(0),  // số mẫu đúng tương ứng
+}
 testSet.forEach(function (item, index) {
     let output = net.run(item.input);
     let numberOut = output.indexOf(Math.max.apply(null, output))
+    detailTest.count[numberOut]++;
     let numberRes = item.output.indexOf(Math.max.apply(null, item.output));
-    if (numberOut == numberRes) OK++;
-
+    if (numberOut == numberRes) {
+        OK++;
+        detailTest.ok[numberOut]++;
+    }
 });
 
 console.log('OK: ', OK);
 console.log('totalSetTest: ', totalSetTest);
 console.log('Percent recognition: ', (OK / totalSetTest) * 100, '%')
-
+for (var i = 0; i < 26; i++) {
+    console.log(arraysChart[i], ': ', detailTest.ok[i] + '/' + detailTest.count[i] + ' -> ' + (detailTest.ok[i] * 100 / detailTest.count[i]) + ' %')
+}
 var obj = net.toJSON();
 jsonfile.writeFile(file, obj, function (err) {
     if (err) throw err;
--- ocr/code/test.js
+++ ocr/code/test.js
@@ -4,46 +4,47 @@ let brain = require('brain');
 const Jimp = require('jimp');
 const width = 10, height = 15;
 const nguong = 20;
+const cv = require('opencv');
 
 const arraysChart = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'k', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// TEST NHẬN DẠNG CHỮ FROM LOCAL KHÔNG CÓ TRONG BỘ TRAIN VÀ TEST
-var json = require('./../net.json');
-let net = new brain.NeuralNetwork();
-net.fromJSON(json);
+// // TEST NHẬN DẠNG CHỮ FROM LOCAL KHÔNG CÓ TRONG BỘ TRAIN VÀ TEST
+// var json = require('./../net.json');
+// let net = new brain.NeuralNetwork();
+// net.fromJSON(json);
 
-const imgSrc = 'tmp/test/processed_tmp_1494496665080.jpg'
-let input = [];
-Jimp.read(imgSrc, function (err, image) {
-    this.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
+// const imgSrc = 'tmp/test/processed_tmp_1494496665080.jpg'
+// let input = [];
+// Jimp.read(imgSrc, function (err, image) {
+//     this.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
 
-        var red = this.bitmap.data[idx + 0];
-        var green = this.bitmap.data[idx + 1];
-        var blue = this.bitmap.data[idx + 2];
-        var alpha = this.bitmap.data[idx + 3];
-        let tbc = (red + green + blue) / 3;
+//         var red = this.bitmap.data[idx + 0];
+//         var green = this.bitmap.data[idx + 1];
+//         var blue = this.bitmap.data[idx + 2];
+//         var alpha = this.bitmap.data[idx + 3];
+//         let tbc = (red + green + blue) / 3;
 
-        if (tbc < nguong) {
-            input.push(0);
-        }
-        else {
-            input.push(1);
-        }
-    }).autocrop(function () {
-        for (var u = width; u < height * width; u += width) {
-            let row = '';
-            for (var v = u - width; v < u; v++) {
-                row += input[v];
-            }
-            console.log(row);
-        }
+//         if (tbc < nguong) {
+//             input.push(0);
+//         }
+//         else {
+//             input.push(1);
+//         }
+//     }).autocrop(function () {
+//         for (var u = width; u < height * width; u += width) {
+//             let row = '';
+//             for (var v = u - width; v < u; v++) {
+//                 row += input[v];
+//             }
+//             console.log(row);
+//         }
 
-        var output = net.run(input);
-        var indexOutput = output.indexOf(Math.max.apply(null, output));
-        console.log('DONE: ', indexOutput, arraysChart[indexOutput]);
-    })
-})
+//         var output = net.run(input);
+//         var indexOutput = output.indexOf(Math.max.apply(null, output));
+//         console.log('DONE: ', indexOutput, arraysChart[indexOutput]);
+//     })
+// })
 
 
 
@@ -86,3 +87,32 @@ Jimp.read(imgSrc, function (err, image) {
 //     console.log();
 //     console.log();
 // });
+
+// TEST TIM CONTUR
+var imgSrc = 'tmp/test/' + 'theboss.jpg';
+cv.readImage(imgSrc, function (err, im) {
+
+    var contours = im.findContours();
+    // Count of contours in the Contours object
+    console.log(contours)
+    contours.size();
+    // Count of corners(verticies) of contour `index`
+    contours.cornerCount(index);
+    // Access vertex data of contours
+    for (var c = 0; c < contours.size(); ++c) {
+        console.log("Contour " + c);
+        for (var i = 0; i < contours.cornerCount(c); ++i) {
+            var point = contours.point(c, i);
+            console.log("(" + point.x + "," + point.y + ")");
+        }
+    }
+    // Computations of contour `index`
+    contours.area(index);
+    contours.arcLength(index, isClosed);
+    contours.boundingRect(index);
+    contours.minAreaRect(index);
+    contours.isConvex(index);
+    // Destructively alter contour `index`
+    contours.approxPolyDP(index, epsilon, isClosed);
+    contours.convexHull(index, clockwise);
+});

927afbcdc01517fdb415c03a5467dca3b5ce6c24
The Boss <tatchu.it@gmail.com>
2017-06-22 19:10:13 +0700

    The Boss: update code training

--- ocr/code/_training.js
+++ ocr/code/_training.js
@@ -1,4 +1,4 @@
-const brain = require('brain.js');        // module hỗ trợ xây dựng mạng
+const brain = require('brain');        // module hỗ trợ xây dựng mạng
 const jsonfile = require('jsonfile')      // module hỗ trợ lưu trữ file json
 const shuffle = require('shuffle-array'); // module hỗ trợ xáo trộn mảng
 
@@ -14,23 +14,22 @@ shuffle(testSet)
 // Khởi tạo mạng
 let net = new brain.NeuralNetwork({
     hiddenLayers: 80,       // nố Neural lớp ẩn
-    learningRate: 0.4       // bước học
+    learningRate: 0.4,       // bước học
 });
 // Tính thời gian huấn luyện
 console.time('train: ')
 net.train(trainingSet, {
     errorThresh: 0.0001,    // ngưỡng lỗi chấp nhận
     momentum: 0.3,          // momentum
-    iterations: 2000,       // số lần huấn luyện tối đa
+    iterations: 200,       // số lần huấn luyện tối đa
+    log: false,
+    logPeriod: 10,
+    callback: function (data) {
+        console.log(data);
+    }
 });
 console.timeEnd('train: ')
 
-// Lưu trữ trọng số mạng
-let file = 'net1.json'
-let obj = net.toJSON();
-jsonfile.writeFile(file, obj, function (err) {
-    if (err) throw err;
-})
 
 // ĐÁNH GIÁ NĂNG LỰC MẠNG
 const arraysChart = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -57,6 +56,27 @@ testSet.forEach(function (item, index) {
 console.log('Test case count: ', totalSetTest);
 console.log('Pass: ', OK);
 console.log('Percent recognition: ', (OK / totalSetTest) * 100, '%')
-for (let i = 0; i < 26; i++) {
-    console.log(`${arraysChart[i]} : ${detailTest.ok[i]} / ${detailTest.count[i]} :\t ${(detailTest.ok[i] * 100 / detailTest.count[i])} %`)
+let testResult = {
+    test_case: totalSetTest,
+    pass: OK,
+    percent_recognition: (OK / totalSetTest) * 100,
+    detail: []
 }
+for (let i = 0; i < 26; i++) {
+    console.log(`${arraysChart[i]} : ${detailTest.ok[i]} / ${detailTest.count[i]}:\t ${(detailTest.ok[i] * 100 / detailTest.count[i])} %`)
+    testResult.detail.push({
+        charecter: arraysChart[i],
+        percent: (detailTest.ok[i] / detailTest.count[i]) * 100
+    })
+}
+
+// Lưu trữ trọng số mạng
+let file = 'net1.json'
+let obj = {
+    test: testResult,
+    net: net.toJSON()
+}
+
+jsonfile.writeFile(file, obj, function (err) {
+    if (err) throw err;
+})

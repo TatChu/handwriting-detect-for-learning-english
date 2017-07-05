14bf5038045e6912a9eced2f3027d4e3aef2edbf
The Boss <tatchu.it@gmail.com>
2017-07-05 10:34:51 +0700

    The Boss:

--- ocr/code/img-process/cat_anh_bang_contour.js
+++ ocr/code/img-process/cat_anh_bang_contour.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const Jimp = require('jimp');
 
 const height_crop = 15, width_crop = 10;
-const nguong = 128;
+const nguong = 210;
 const whitePixel = Jimp.rgbaToInt(255, 255, 255, 255);
 const blackPixel = Jimp.rgbaToInt(0, 0, 0, 255);
 const WHITE = [255, 255, 255];
@@ -13,11 +13,13 @@ const thickness = 1;
 const Promise = require("bluebird");
 
 require('async-arrays').proto();
-
+const arrayChart = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'k', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 module.exports = {
     PreProcess,
 }
+
 
 function PreProcess(imgDir, imgName, data) {
     return new Promise(function (resolve, reject) {
@@ -25,7 +27,7 @@ function PreProcess(imgDir, imgName, data) {
             if (err) {
                 return reject(err);
             }
-            imgJimp.getBuffer(Jimp.AUTO, function (err, buff) {
+            imgJimp.resize(3000, 2000).getBuffer(Jimp.AUTO, function (err, buff) {
                 if (err) {
                     return reject(err);
                 }
@@ -36,7 +38,6 @@ function PreProcess(imgDir, imgName, data) {
                     const width = img.width();
                     const height = img.height();
 
-                    let imgSave = img.copy();
 
                     if (width < 1 || height < 1) {
                         reject(new Error('Image has no size'));
@@ -52,16 +53,16 @@ function PreProcess(imgDir, imgName, data) {
                     // img.gaussianBlur([1, 5]);
                     img.gaussianBlur([3, 3]);
                     // img.save(imgDir + '2_' + imgName);
-
+                    let imgSave = img.copy();
 
                     //  Xac dinh duong net
                     const lowThresh = data.lowThresh || 0; // càng lớn càng mất nét
-                    const highThresh = data.highThresh || 120; // cang lon loc cang manh
+                    const highThresh = data.highThresh || 100; // cang lon loc cang manh
                     img.canny(lowThresh, highThresh);
-                    // img.save(imgDir + '3_' + imgName);
+                    img.save(imgDir + '/_1' + imgName);
 
                     // lam beo chu
-                    const iterations = 1; // cang lon chu cang beo'
+                    const iterations = 2; // cang lon chu cang beo'
                     img.dilate(iterations);
                     img.erode(1); // xói mòn
                     // img.save(imgDir + '4_' + imgName);
@@ -75,7 +76,7 @@ function PreProcess(imgDir, imgName, data) {
                     for (let i = 0; i < contours.size(); i++) {
                         let bound = contours.boundingRect(i);
 
-                        if (bound.width > 5 && bound.height > 5) {
+                        if (bound.width > 5 && bound.height > 5 && bound.width < 180 && bound.height < 180) {
                             listAllBound.push(bound);
                         }
                     }
@@ -126,20 +127,55 @@ function PreProcess(imgDir, imgName, data) {
                         if (item) {
 
                             var imgCrop = imgSave.crop(item.x, item.y, item.width, item.height);
+                            imgSave.rectangle([item.x, item.y], [item.width, item.height], GREEN, 1);
 
                             imgCrop.resize(width_crop, height_crop);
                             let newName = imgName + '_' + index + '.jpg';
-                            var path_save = data.dirDist + newName;
+                            var path_save = data.dirDist + '/' + newName;
 
-                            imgCrop.save(path_save);
-                            listImageCroped.push({
-                                image: newName,
-                                detail: item
-                            });
-                            imgSave.rectangle([item.x, item.y], [item.width, item.height], GREEN, 1);
+                            Jimp.read(imgCrop.toBuffer(), function (err, image) {
 
+                                this.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
+
+                                    var red = this.bitmap.data[idx + 0];
+                                    var green = this.bitmap.data[idx + 1];
+                                    var blue = this.bitmap.data[idx + 2];
+                                    var alpha = this.bitmap.data[idx + 3];
+                                    let tbc = (red + green + blue) / 3;
+                                    // chuyen trang den
+                                    if (tbc < nguong) {
+                                        image.setPixelColor(blackPixel, x, y);
+                                    }
+                                    else {
+                                        image.setPixelColor(whitePixel, x, y);
+                                    }
+                                }).write(path_save, function () {
+                                    // console.log(i, j, name, input.length);
+
+                                    // dataTrain.input.push(input);
+                                    // dataTrain.output.push(output);
+                                    // for (var u = size; u < size * size; u += size) {
+                                    //     let row = '';
+                                    //     for (var v = u - size; v < u; v++) {
+                                    //         row += input[v];
+                                    //     }
+                                    //     console.log(row);
+                                    // }
+                                    listImageCroped.push({
+                                        image: newName,
+                                        detail: item
+                                    });
+                                    // imgCrop.save(path_save);
+
+                                    listImageCroped.push({
+                                        image: newName,
+                                        detail: item
+                                    });
+                                })
+                            })
                         }
                     })
+                    // Lưu để xem biên
                     let nameImageCountour = 'contour_' + imgName;
                     imgSave.save(imgDir + nameImageCountour);
 
@@ -152,8 +188,19 @@ function PreProcess(imgDir, imgName, data) {
         })
     })
 }
-PreProcess('test/', 'vietnamese.jpg', {
-    dirDist: 'test/'
-}).then(res => {
-    console.log(res.listImage)
+// PreProcess('test/', 'vietnamese.jpg', {
+//     dirDist: 'test/'
+// }).then(res => {
+//     console.log(res.listImage)
+// })
+
+arrayChart.forEachEmission((character, index, done) => {
+    PreProcess('image/bang chu cai/', character + '.jpg', {
+        dirDist: 'image/output/' + character
+    }).then(function (res) {
+        console.log('Done: ' + character)
+        done();
+    })
+}, function () {
+    console.log('DONE');
 })
